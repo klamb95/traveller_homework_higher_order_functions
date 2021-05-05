f30ddc47342c9e02147721c9263d6ce447451ead
klamb95 <klamb1995@gmail.com>
2021-05-05 16:37:19 +0100

    get journey's end location

--- models/traveller.js
+++ models/traveller.js
@@ -9,6 +9,9 @@ Traveller.prototype.getJourneyStartLocations = function() {
 };
 
 Traveller.prototype.getJourneyEndLocations = function () {
+  return this.journeys.map((journey) => {
+    return journey.endLocation;
+  })
 
 };
 
