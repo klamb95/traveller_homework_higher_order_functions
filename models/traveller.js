47df281f209963a2f3b8c46fd9313bef316d0a42
klamb95 <klamb1995@gmail.com>
2021-05-05 16:41:39 +0100

    journey by min distance working

--- models/traveller.js
+++ models/traveller.js
@@ -22,6 +22,9 @@ Traveller.prototype.getJourneysByTransport = function (transport) {
 };
 
 Traveller.prototype.getJourneysByMinDistance = function (minDistance) {
+  return this.journeys.filter((journey) =>{
+    return journey.distance >= minDistance;
+  })
 
 };
 
