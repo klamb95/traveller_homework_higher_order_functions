42ba2e0c84d624e7416048a71d3996e637d5488e
klamb95 <klamb1995@gmail.com>
2021-05-06 09:25:14 +0100

    total distance working

--- models/traveller.js
+++ models/traveller.js
@@ -29,10 +29,14 @@ Traveller.prototype.getJourneysByMinDistance = function (minDistance) {
 };
 
 Traveller.prototype.calculateTotalDistanceTravelled = function () {
+  return this.journeys.reduce((total, journey)=> {
+    return total += journey.distance;
+  }, 0);
 
 };
 
 Traveller.prototype.getUniqueModesOfTransport = function () {
+  
 
 };
 
